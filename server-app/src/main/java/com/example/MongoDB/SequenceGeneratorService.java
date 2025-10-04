ed8c60231b4ffce528271f8ec89f5c89c900feb1
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-04 22:47:55 +0200

    Stworzenie tabeli z użytkownikami. Wraz z endpointami do do tworzenia, edycji oraz zapisu. Tworzenie uzytkownika z automatyczną inktrementacją id

---
+++ server-app/src/main/java/com/example/MongoDB/SequenceGeneratorService.java
@@ -0,0 +1,36 @@
+package com.example.MongoDB;
+
+import com.example.kolekcje.Counter;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+@Service
+public class SequenceGeneratorService {
+
+    private final MongoOperations mongoOperations;
+
+    public SequenceGeneratorService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public int getNextSequence(String seqName) {
+        Counter counter = mongoOperations.findAndModify(
+                Query.query(where("_id").is(seqName)),
+                new Update().inc("seq", 1),
+                Counter.class
+        );
+
+        if (counter == null) {
+            counter = new Counter();
+            counter.setId(seqName);
+            counter.setSeq(1);
+            mongoOperations.save(counter);
+            return 1;
+        }
+
+        return counter.getSeq();
+    }
+}
