b73fff5b2417f3f5c4454f6e0acf422a14dce277
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-10 01:20:38 +0100

    Edycja daty w formularzu po naciśnięciu ikony z prawej strony

--- BalansApp/app/src/main/java/com/example/firstcomposeap/ui/components/profile/profileTab/EditUserDialog.kt
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/ui/components/profile/profileTab/EditUserDialog.kt
@@ -3,9 +3,15 @@ package com.example.firstcomposeap.ui.components.profile.profileTab
 import android.app.DatePickerDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -14,11 +20,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.balansapp.ui.service.data.Uzytkownik
 import com.example.firstcomposeap.ui.components.getFormOnlyDate
+import com.example.firstcomposeap.ui.components.icon.Edit_calendar
 import java.time.LocalDate
 import java.util.Calendar
 
@@ -94,16 +103,25 @@ fun EditUserDialog(
                     modifier = Modifier.fillMaxWidth(),
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
                 )
+                Row (modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    OutlinedTextField(
+                        value = dataUrodzenia,
+                        onValueChange = { datePickerDialog.show() },
+                        label = { Text("Data urodzenia") },
+                        modifier = Modifier.weight(1f)
+                            .clickable { datePickerDialog.show() },
+                        readOnly = true
+                    )
+                    IconButton(onClick = {datePickerDialog.show()},
+                        modifier = Modifier.size(38.dp)) {
+                        Icon ( imageVector = Edit_calendar, contentDescription = "wybierz date")
+                    }
+                }
 
-                OutlinedTextField(
-                    value = dataUrodzenia,
-                    onValueChange = { datePickerDialog.show() },
-                    label = { Text("Data urodzenia") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { datePickerDialog.show() },
-                    readOnly = true
-                )
             }
         },
         confirmButton = {
