23ececd484403b2528688547e82c50a834de7120
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-21 23:07:31 +0100

    Dodanie endpointów obsługi posiłków (puste)

--- server-app/src/main/java/com/example/controllers/ProduktController.java
+++ server-app/src/main/java/com/example/controllers/ProduktController.java
@@ -1,11 +1,15 @@
 package com.example.controllers;
 
 import com.example.kolekcje.posilki.Produkt;
+import com.example.kolekcje.uzytkownik.Uzytkownik;
+import com.example.requests.MealUpdate;
 import com.example.services.ProduktService;
+import com.example.services.UzytkownikService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +19,11 @@ import java.util.Optional;
 @RequestMapping("/api/produkty")
 public class ProduktController {
     private final ProduktService produktService;
+    private final UzytkownikService uzytkownikService;
 
-    public ProduktController(ProduktService produktService) {
+    public ProduktController(ProduktService produktService, UzytkownikService uzytkownikService) {
         this.produktService = produktService;
+        this.uzytkownikService = uzytkownikService;
     }
 
 
@@ -56,6 +62,30 @@ public class ProduktController {
     @GetMapping("/produkt/kod/{kodKreskowy}")
     public Optional<Produkt> findByKodKreskowy(@PathVariable String kodKreskowy) {
         return produktService.findByKodKreskowy(kodKreskowy);
+    }
+
+
+    @GetMapping("/posilek")
+    public Optional<MealUpdate> getMeal( @RequestBody MealUpdate update ) {
+        return Optional.empty();
+    }
+
+    @PostMapping("/posilek")
+    public ResponseEntity<?> updateMeal(@RequestBody MealUpdate update, Authentication authentication) {
+        if( authentication == null ) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Brak autoryzacji");
+        }
+        String userEmail = authentication.getName();
+        Optional<Uzytkownik> optUsr = uzytkownikService.getUserByEmail(userEmail);
+        if( optUsr.isPresent() ) {
+            Uzytkownik usr = optUsr.get();
+
+
+
+
+            return ResponseEntity.ok().body("Brak autoryzacji");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Brak autoryzacji");
     }
 
 
