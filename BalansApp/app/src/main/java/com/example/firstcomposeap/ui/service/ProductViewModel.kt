c0c8bfe9c99ce4c09f43a909e3502c66a3996380
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-22 16:29:59 +0100

    Przeniesienie mapy mapy z widoku do produkViewModel uwspólniajac go dla wszystkich

--- BalansApp/app/src/main/java/com/example/firstcomposeap/ui/service/ProductViewModel.kt
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/ui/service/ProductViewModel.kt
@@ -3,11 +3,13 @@ package com.example.firstcomposeap.ui.service
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.balansapp.ui.service.ApiClient
+import com.example.firstcomposeap.ui.service.data.MealGroup
 import com.example.firstcomposeap.ui.service.data.PoraDnia
 import com.example.firstcomposeap.ui.service.data.Produkt
 import kotlinx.coroutines.launch
@@ -18,9 +20,20 @@ class ProductViewModel : ViewModel() {
     var message by mutableStateOf<String?>(null)
 
 
-    var selectedProducts =   mutableStateListOf<Produkt>() // wypełniany w widoku szukania produktów (SearchProductScreen za  pośrednictwem zmiennej consumeProduct) przekazywany do widoku posiłków w ciuągu dnia i tam czyszczony
-    var isReadyToSend = mutableStateOf(false)
+    val mealsMap = mutableStateMapOf<PoraDnia, MealGroup>(
+        PoraDnia.SNIADANIE to MealGroup(PoraDnia.SNIADANIE, mutableStateListOf()),
+        PoraDnia.LUNCH to MealGroup(PoraDnia.LUNCH, mutableStateListOf()),
+        PoraDnia.OBIAD to MealGroup(PoraDnia.OBIAD, mutableStateListOf()),
+        PoraDnia.KOLACJA to MealGroup(PoraDnia.KOLACJA, mutableStateListOf()),
+        PoraDnia.PRZEKASKA to MealGroup(PoraDnia.PRZEKASKA, mutableStateListOf())
+    )
+
+    var selectedProducts =   mutableStateListOf<Produkt>() // wypełniany w widoku szukania produktów (SearchProductScreen za  pośrednictwem zmiennej consumeProduct) przekazywany do widoku posiłków w ciuągu dnia i tam czyszczony dane do dodania usuwanie przez przycisk w oknie
+    var isSelectedProductsReadyToSend = mutableStateOf(false)
     var selectedDayTime = mutableStateOf(PoraDnia.CLEAR)
+
+    var selectedRecipes =   mutableStateListOf<List<Produkt>>()
+    var isSelectedRecipesReadyToSend = mutableStateOf(false)
 
 
     fun addNewProduct(product: Produkt) {
