8ea8f4fa26488d3c9077160ca75afee84ff9a6ec
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-29 21:40:01 +0100

    Usunięcie warnings z MainActivity.kt

--- BalansApp/app/src/main/java/com/example/firstcomposeap/MainActivity.kt
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.example.firstcomposeap
+package com.example.balansapp
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
