905ee1c45c7fe78dfc90fd743ebdb50dc430b3a3
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-30 22:37:23 +0100

    Widok tworzenia nowego ćwiczenia

---
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/ui/screens/NewExerciseScreen.kt
@@ -0,0 +1,14 @@
+package com.example.firstcomposeap.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.firstcomposeap.ui.service.TreningViewModel
+
+
+
+@Composable
+fun NewExerciseScreen(treningViewModel: TreningViewModel ) {
+    Column {
+
+    }
+}
