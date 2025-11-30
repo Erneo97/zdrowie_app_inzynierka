f1969b4a2b0a5c83f7ca82ac00d46a01b445deb8
Erneo97 <michalkaniewski1997@gmail.com>
2025-11-30 22:37:36 +0100

    Widok tworzenia nowego planu treningowego

---
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/ui/screens/NewTreningPlanScreen.kt
@@ -0,0 +1,13 @@
+package com.example.firstcomposeap.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.firstcomposeap.ui.service.TreningViewModel
+
+
+@Composable
+fun NewTreningPlanScreen(treningViewModel: TreningViewModel ) {
+    Column {
+
+    }
+}
