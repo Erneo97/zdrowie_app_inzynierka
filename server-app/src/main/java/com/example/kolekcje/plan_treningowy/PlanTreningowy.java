e77e47fab3cb836c872f2794a9ecba181b0c4db1
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-13 00:25:25 +0100

    Rozszerzona wersja planu treningowego cwiczenia w planie i cel planu

--- server-app/src/main/java/com/example/kolekcje/plan_treningowy/PlanTreningowy.java
+++ server-app/src/main/java/com/example/kolekcje/plan_treningowy/PlanTreningowy.java
@@ -1,16 +1,24 @@
 package com.example.kolekcje.plan_treningowy;
 
+import com.example.kolekcje.enumy.GOAL;
+import dev.morphia.annotations.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 @Document(collection = "PlanTreningowy")
 public class PlanTreningowy {
-    @id
+    @Id
     private int id;
     private int id_uzytkownia;
     Date dataUtworzenia;
     String nazwa;
+    List<CwiczeniaPlanuTreningowego> cwiczeniaPlanuTreningowe;
+    GOAL cel;
+
+    public GOAL getCel() { return cel; }
+    public void setCel(GOAL goal) {this.cel = goal;}
 
     public PlanTreningowy() { }
     public int getId() {return id;}
@@ -22,4 +30,7 @@ public class PlanTreningowy {
     public void setId_uzytkownia(int id_uzytkownia) {this.id_uzytkownia = id_uzytkownia;}
     public void setNazwa(String nazwa) {this.nazwa = nazwa;}
     public void setDataUtworzenia(Date data) { dataUtworzenia = data; }
+
+    public List<CwiczeniaPlanuTreningowego> getCwiczeniaPlanuTreningowe() {return cwiczeniaPlanuTreningowe;}
+    public void setCwiczeniaPlanuTreningowe(List<CwiczeniaPlanuTreningowego> cwiczeniaPlanuTreningowe) {this.cwiczeniaPlanuTreningowe = cwiczeniaPlanuTreningowe;}
 }
