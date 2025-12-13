8fa1682a8f0a504708f019d291f68294f3f5456a
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-13 04:29:58 +0100

    Znajdowanie planu treningowego o idUżytkownika

--- server-app/src/main/java/com/example/repositories/TreningPlanRepository.java
+++ server-app/src/main/java/com/example/repositories/TreningPlanRepository.java
@@ -4,10 +4,13 @@ import com.example.kolekcje.plan_treningowy.PlanTreningowy;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @RepositoryRestResource(collectionResourceRel = "planTreningowy", path  = "planTreningowy" )
 public interface TreningPlanRepository extends MongoRepository<PlanTreningowy, String> {
     Optional<PlanTreningowy> findById(int id);
+
+    List<PlanTreningowy> findByIdUzytkownia(int idUzytkownia);
 }
