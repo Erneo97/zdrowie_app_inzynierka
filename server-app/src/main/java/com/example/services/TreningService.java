84603d0289311de730a6b3697a8216f5fa7bc074
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-13 01:37:32 +0100

    logika pobierania skrótowych kart wszystkich treningów użytkownika

--- server-app/src/main/java/com/example/services/TreningService.java
+++ server-app/src/main/java/com/example/services/TreningService.java
@@ -1,19 +1,28 @@
 package com.example.services;
 
+import com.example.controllers.UzytkownikController;
 import com.example.kolekcje.enumy.LicznikiDB;
 import com.example.kolekcje.plan_treningowy.Cwiczenie;
 import com.example.kolekcje.plan_treningowy.PlanTreningowy;
+import com.example.kolekcje.plan_treningowy.TreningsPlanCard;
 import com.example.kolekcje.posilki.ProduktyDoPotwierdzenia;
 import com.example.kolekcje.uzytkownik.Uzytkownik;
 import com.example.repositories.CwiczeniaRepository;
 import com.example.repositories.PotwierdzProduktyRepository;
 import com.example.repositories.TreningPlanRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class TreningService {
@@ -23,16 +32,19 @@ public class TreningService {
     private final TreningPlanRepository treningPlanRepository;
     private final SequenceGeneratorService sequenceGenerator;
     private final UzytkownikService uzytkownikService;
+    private final MongoTemplate mongoTemplate;
 
     public TreningService(CwiczeniaRepository cwiczeniaRepository,
                           TreningPlanRepository treningPlanRepository,
                           PotwierdzProduktyRepository potwierdzProduktyRepository,
+                          MongoTemplate mongoTemplate,
                           SequenceGeneratorService sequenceGenerator, UzytkownikService uzytkownikService) {
         this.cwiczeniaRepository = cwiczeniaRepository;
         this.treningPlanRepository = treningPlanRepository;
         this.potwierdzProduktyRepository = potwierdzProduktyRepository;
         this.sequenceGenerator = sequenceGenerator;
         this.uzytkownikService = uzytkownikService;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public Cwiczenie createExercise(Cwiczenie cwiczenia) {
@@ -68,6 +80,33 @@ public class TreningService {
             uzytkownik.setAktualnyPlan(nowyPlan.getId());
             uzytkownikService.updateUser(uzytkownik);
         }
+    }
+    private static final Logger log = LoggerFactory.getLogger(TreningService.class);
+    public List<TreningsPlanCard> getAllTreningPlans(Uzytkownik uzytkownik) {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id_uzytkownia").in(uzytkownik.getId()));
+
+        List<PlanTreningowy> planyUzytkownika = mongoTemplate.find(query, PlanTreningowy.class, "PlanTreningowy");
+
+        List<TreningsPlanCard> treningPlanCards = new ArrayList<>();
+
+        planyUzytkownika.forEach(item -> {
+            TreningsPlanCard nowy = new TreningsPlanCard();
+            nowy.setId(item.getId());
+            nowy.setSeasonName(item.getNazwa());
+            nowy.setIsActive( (uzytkownik.getAktualnyPlan() == item.getId()));
+            log.info("tp  " + uzytkownik.getAktualnyPlan() + " " + item.getId()+ "   " + (uzytkownik.getAktualnyPlan() == item.getId()));
+            nowy.setGoal(item.getCel());
+            nowy.setStartDate(item.getDataUtworzenia().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString());
+
+            nowy.setEndDate(item.getDataUtworzenia().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().toString()); // TODO: zmienić na datę ostatniego treningu
+            nowy.setTrainingCount(0); // TODO; dodać zliczanie treningów danego planu
+
+            treningPlanCards.add(nowy);
+        });
+
+        return treningPlanCards;
     }
 
 }
