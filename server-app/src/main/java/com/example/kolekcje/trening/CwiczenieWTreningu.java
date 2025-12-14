1da7200513fd04d74364dc694cdd3313df2c4f9b
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-14 22:54:01 +0100

    Klasa przechowująca dane danego ćwiczenia w odbywanym / odbytym treningu.

---
+++ server-app/src/main/java/com/example/kolekcje/trening/CwiczenieWTreningu.java
@@ -0,0 +1,25 @@
+package com.example.kolekcje.trening;
+
+import com.example.kolekcje.plan_treningowy.Seria;
+
+import java.util.List;
+
+public class CwiczenieWTreningu {
+    int id;
+    String nazwa, czas; //  czas w formacie mm:ss
+    List<Seria> serie;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {this.id = id;}
+
+    public String getNazwa() {return nazwa;}
+    public void setNazwa(String nazwa) {this.nazwa = nazwa;}
+
+    public String getCzas() {return czas;}
+    public void setCzas(String czas) {this.czas = czas;}
+
+    public List<Seria> getSerie() {return serie;}
+    public void setSerie(List<Seria> serie) {this.serie = serie;}
+}
