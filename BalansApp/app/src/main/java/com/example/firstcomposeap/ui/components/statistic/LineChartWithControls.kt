1a07527809d01527de392c675c42dc7a00d6909f
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-17 21:48:20 +0100

    usuniecie import

--- BalansApp/app/src/main/java/com/example/firstcomposeap/ui/components/statistic/LineChartWithControls.kt
+++ BalansApp/app/src/main/java/com/example/firstcomposeap/ui/components/statistic/LineChartWithControls.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
