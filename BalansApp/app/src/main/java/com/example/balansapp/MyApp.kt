d34502f7427f84f1a3f404975cc0bd66cf10c599
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-20 23:15:01 +0100

    Stworzenie kanału komunikacji do powiadomień

--- BalansApp/app/src/main/java/com/example/balansapp/MyApp.kt
+++ BalansApp/app/src/main/java/com/example/balansapp/MyApp.kt
@@ -1,34 +1,53 @@
 package com.example.balansapp
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.firstcomposeap.ui.notification.ReminderWorker
 import java.util.concurrent.TimeUnit
 
 class MyApp : Application() {
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "BalansApp Channel"
+            val descriptionText = "Kanał powiadomień przypomnień"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("BalansApp Channel", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
-        val workRequest = PeriodicWorkRequestBuilder<ReminderWorker>(
-            6, TimeUnit.HOURS
-        )
-            .setInitialDelay(3, TimeUnit.HOURS)
-            .build()
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "training_and_meal_reminder",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-        )
-
-
-//        val testRequest = OneTimeWorkRequestBuilder<ReminderWorker>()
-//            .setInitialDelay(10, TimeUnit.SECONDS)
+        createNotificationChannel()
+//        val workRequest = PeriodicWorkRequestBuilder<ReminderWorker>(
+//            6, TimeUnit.HOURS
+//        )
+//            .setInitialDelay(3, TimeUnit.HOURS)
 //            .build()
-//
-//        WorkManager.getInstance(this).enqueue(testRequest)
+//        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+//            "training_and_meal_reminder",
+//            ExistingPeriodicWorkPolicy.KEEP,
+//            workRequest
+//        )
+
+
+
+
+        val testRequest = OneTimeWorkRequestBuilder<ReminderWorker>()
+            .setInitialDelay(25, TimeUnit.SECONDS)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(testRequest)
 
     }
 }
