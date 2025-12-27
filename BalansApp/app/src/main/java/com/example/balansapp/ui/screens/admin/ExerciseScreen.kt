0b78f48eacff2dd3f33491b2dd3577a2dfe8ab64
Erneo97 <michalkaniewski1997@gmail.com>
2025-12-27 15:16:10 +0100

    Logo do zakładki produkty i ćwiczenia admin

--- BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ExerciseScreen.kt
+++ BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ExerciseScreen.kt
@@ -1,6 +1,7 @@
 package com.example.balansapp.ui.screens.admin
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -9,10 +10,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import com.example.balansapp.R
+import com.example.balansapp.ui.components.input.LogoBackGround
 import com.example.balansapp.ui.navigation.main.MainLayoutAdmin
 
 @Composable
@@ -30,7 +33,18 @@ fun ExerciseAdminScreen(navController: NavHostController ) {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            Text("ExerciseAdminScreen")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                LogoBackGround()
+            }
+
+            Column {
+                Text("ExerciseAdminScreen")
+
+            }
         }
     }
 }
--- BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ProductAdminScreen.kt
+++ BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ProductAdminScreen.kt
@@ -12,12 +12,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.balansapp.R
 import com.example.balansapp.ui.components.FullSizeButton
+import com.example.balansapp.ui.components.input.LogoBackGround
 import com.example.balansapp.ui.navigation.main.MainLayoutAdmin
 import com.example.balansapp.ui.navigation.main.Screen
 
@@ -42,6 +44,13 @@ fun ProductAdminScreen(navController: NavHostController ) {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                LogoBackGround()
+            }
 
             Column {
                 FullSizeButton(
