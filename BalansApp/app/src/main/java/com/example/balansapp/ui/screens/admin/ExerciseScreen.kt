931d385df79bebe1c501771b82b775488cfe9c83
Erneo97 <michalkaniewski1997@gmail.com>
2026-01-11 21:26:57 +0100

    Wyszukiwarka ćwiczeń dla admina

--- BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ExerciseScreen.kt
+++ BalansApp/app/src/main/java/com/example/balansapp/ui/screens/admin/ExerciseScreen.kt
@@ -1,15 +1,23 @@
 package com.example.balansapp.ui.screens.admin
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -17,6 +25,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,11 +34,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.balansapp.R
 import com.example.balansapp.ui.components.FullSizeButton
@@ -40,8 +52,11 @@ import com.example.balansapp.ui.navigation.main.Screen
 import com.example.balansapp.ui.service.AdminVievModel
 import com.example.firstcomposeap.ui.components.icon.Delete
 import com.example.firstcomposeap.ui.components.icon.Done
+import com.example.firstcomposeap.ui.components.treningplans.MuscleGroupFilter
+import com.example.firstcomposeap.ui.service.SearchViewModel
 import com.example.firstcomposeap.ui.service.TreningViewModel
 import com.example.firstcomposeap.ui.service.data.Cwiczenie
+import com.example.firstcomposeap.ui.service.data.GrupaMiesniowa
 
 @Composable
 fun ExerciseAdminScreen(navController: NavHostController,
@@ -49,6 +64,17 @@ fun ExerciseAdminScreen(navController: NavHostController,
                         adminVievModel: AdminVievModel ) {
     val context = LocalContext.current
     var selectedItem by remember { mutableStateOf(context.getString(R.string.exercises)) }
+    val searchViewModel: SearchViewModel = viewModel()
+
+    var selectedGroups by remember { mutableStateOf(listOf<GrupaMiesniowa>()) }
+    var accurately by remember { mutableStateOf(false) } // czy dokładne odwzorowanie tagów czy tylko te zawierające
+
+    val suggestions = searchViewModel.suggestionsList
+
+    var isFocused by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    val query = searchViewModel.searchQuery
+
 
     LaunchedEffect(Unit, adminVievModel.loadingData) {
         adminVievModel.downloadExercisesToConfirmeList()
@@ -123,11 +149,81 @@ fun ExerciseAdminScreen(navController: NavHostController,
                         }
 
                     }
-                    1 -> { }
+                    1 -> {
+                        TextField(
+                            value = query,
+                            onValueChange = { searchViewModel.onSearchQueryChange(it, false, selectedGroups, accurately) },
+                            placeholder = { Text("Nazwa ćwiczenia...") },
+                            singleLine = true,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .onFocusChanged { focusState ->
+                                    isFocused = focusState.isFocused
+                                }
+                        )
+                        AnimatedVisibility(visible = suggestions.isNotEmpty() && isFocused) {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 4.dp),
+                                shape = RoundedCornerShape(8.dp),
+                                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                            ) {
+
+                                LazyColumn(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.White)
+                                ) {
+                                    items(suggestions) { item ->
+                                        Text(
+                                            text = item,
+                                            modifier = Modifier
+                                                .clickable {
+                                                    searchViewModel.selectSuggestion(item)
+                                                    // pobranie cwiczen
+                                                    focusManager.clearFocus()
+                                                    isFocused = false
+                                                }
+                                                .padding(12.dp)
+                                        )
+                                        HorizontalDivider(
+                                            Modifier,
+                                            DividerDefaults.Thickness,
+                                            DividerDefaults.color
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        MuscleGroupFilter(
+                            selected = selectedGroups,
+                            onSelectedChange = { groups ->
+                                selectedGroups = groups
+                                searchViewModel.onSearchQueryChange(isProduct =  false, groupMuscle = selectedGroups, precision =  accurately)
+                                isFocused = true
+                            },
+                            onChangePrecision = {
+                                accurately = it
+                                searchViewModel.onSearchQueryChange(isProduct =  false, groupMuscle = selectedGroups, precision =  accurately)
+                                isFocused = true
+                            }
+                        )
+
+
+
+                        ExerciseAdminTab(  )
+
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun ExerciseAdminTab(  ) {
+
 }
 
 
